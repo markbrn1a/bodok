2f7d9aa40d50bd16576275caacb56c3e5c8f287a
markbrn1a <httpx.eu@gmail.com>
2024-08-14 00:15:38 +0200

    css

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,42 @@
 import { Inter } from "next/font/google";
 import Layout from "@/components/layout/Layout";
-import "./globals.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "../public/assets/css/bootstrap.min.css";
-import "../public/assets/css/animate.min.css";
-import "../public/assets/css/magnific-popup.css";
-import "../public/assets/css/fontawesome-all.min.css";
-import "../public/assets/css/odometer.css";
-import "../public/assets/css/tg-cursor.css";
-import "../public/assets/css/default.css";
-import "../public/assets/css/jarallax.css";
-import "../public/assets/css/style.css";
-import "../public/assets/css/responsive.css";
-import DataBg from "@/components/elements/DataBg";
-import { headers } from "next/headers";
+// import "./globals.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import "../public/assets/css/bootstrap.min.css";
+// import "../public/assets/css/animate.min.css";
+// import "../public/assets/css/magnific-popup.css";
+// import "../public/assets/css/fontawesome-all.min.css";
+// import "../public/assets/css/odometer.css";
+// import "../public/assets/css/tg-cursor.css";
+// import "../public/assets/css/default.css";
+// import "../public/assets/css/jarallax.css";
+// import "../public/assets/css/style.css";
+// import "../public/assets/css/responsive.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+
+import dynamic from "next/dynamic";
+
+const dynamicCSS = [
+  "globals.css",
+  "slick-carousel/slick/slick.css",
+  "slick-carousel/slick/slick-theme.css",
+  "../public/assets/css/bootstrap.min.css",
+  "../public/assets/css/animate.min.css",
+  "../public/assets/css/magnific-popup.css",
+  "../public/assets/css/fontawesome-all.min.css",
+  "../public/assets/css/odometer.css",
+  "../public/assets/css/tg-cursor.css",
+  "../public/assets/css/default.css",
+  "../public/assets/css/jarallax.css",
+  "../public/assets/css/style.css",
+  "../public/assets/css/responsive.css",
+];
+
+dynamicCSS.forEach((css) => {
+  dynamic(() => import(css));
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
